de78679fe2f93b79767bd283030a77bbc8c0cf5a
davidbzhao <dz6hu@virginia.edu>
2019-03-15 17:12:00 -0400

    Fix handler detach bug

--- app/judging/main/static/js/prejudging.js
+++ app/judging/main/static/js/prejudging.js
@@ -1,8 +1,8 @@
-$('#import-categories').submit(function(){
+$(document).on('submit', '#import-categories', function(){
     let url = $(this).attr('action');
     let data = $(this).serialize();
     $.post(url, data, function(response) {
         $('#edit-categories-container').empty().append(response);
     })
     return false;
-})
+});
